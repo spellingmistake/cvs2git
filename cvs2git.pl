345532627cc2d0ff603f42d73365223aefe6c65d
Thomas Egerer <hakke_007@gmx.de>
2011-08-24 10:07:20 +0200

    cvs2git.pl: Add licencing information

--- cvs2git.pl
+++ cvs2git.pl
@@ -1,5 +1,22 @@
 #!/usr/bin/perl
 
+################################################################################
+#                                                                              #
+# Copyright (C) 2011 Thomas Egerer and Torsten Hilbrich, secunet Security      #
+# Networks AG                                                                  #
+#                                                                              #
+# This program is free software; you can redistribute it and/or modify it      #
+# under the terms of the GNU General Public License as published by the        #
+# Free Software Foundation; either version 2 of the License, or (at your       #
+# option) any later version.  See <http://www.fsf.org/copyleft/gpl.txt>.       #
+#                                                                              #
+# This program is distributed in the hope that it will be useful, but          #
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY   #
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License     #
+# for more details.                                                            #
+#                                                                              #
+################################################################################
+
 use strict;
 use warnings;
 use Data::Dumper qw(Dumper);
